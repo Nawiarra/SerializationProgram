c609a867588b7dfbb9bbaf0beec9af01cd301363
Nawiarra <nika.berezovskaya@ukr.net>
2020-10-06 21:16:19 +0300

    Add serialization and deserialization metod's

--- SerializationProgram/SerializationCore/Serialization.cs
+++ SerializationProgram/SerializationCore/Serialization.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
 
 namespace SerializationCore
 {
-    public class Serialization
+    public class Serialization <T>
     {
+        public static void Serialize(T obj, string name)
+        {
+            using (FileStream fs = new FileStream(name, FileMode.OpenOrCreate))
+            {
+                JsonSerializer.SerializeAsync<T>(fs, obj);
+            }
+        }
+
+        public static T Deserialize(string name)
+        {
+            using (FileStream fs = new FileStream(name, FileMode.OpenOrCreate))
+            {
+                T result = JsonSerializer.Deserialize<T>(name);
+                return result;
+            }
+        }
     }
+
 }
